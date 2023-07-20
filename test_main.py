f2617ba57f9664bf4e3962a58b57c07211d0fa1d
Wiktor Kwapisiewicz <wiktor@metacode.biz>
2023-07-20 13:54:21 +0200

    Fix waiting for a response in thread

--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
 from main import Window
 from PySide6 import QtCore
 from http.client import HTTPConnection
+import threading
 
 
 def test_no_op(qtbot):
@@ -17,6 +18,18 @@ def test_start_server(qtbot):
     conn.request("GET", "/")
 
     print("Before")
-    resp = conn.getresponse()
+
+    resp = None
+
+    def get_response_in_thread():
+        nonlocal resp
+        resp = conn.getresponse()
+
+    t = threading.Thread(target=get_response_in_thread)
+    t.start()
+
+    # waitUntil() waits for the response to arrive,
+    # while keeping the Qt event loop running.
+    qtbot.waitUntil(lambda: resp is not None)
     print("After")
     assert resp.status == 200
